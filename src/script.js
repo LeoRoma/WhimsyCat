f98b9b713027ddbcca5e4b1b9b0c6912628437f7
LeoRoma <xiajtn@gmail.com>
2021-03-16 16:03:51 +0000

    Fetched cat api

---
+++ src/script.js
@@ -0,0 +1,17 @@
+const landingPageAlternate = document.querySelectorAll('.landingpage-alternate');
+console.log(landingPageAlternate[1])
+
+
+landingPageAlternate[0].addEventListener('mouseover', () => {
+    landingPageAlternate[0].style.opacity = "0";
+    landingPageAlternate[1].style.opacity = "1";
+})
+
+landingPageAlternate[0].addEventListener('mouseout', () => {
+    landingPageAlternate[0].style.opacity = "1";
+    landingPageAlternate[1].style.opacity = "0";
+})
+
+fetch("https://api.thecatapi.com/v1/breeds?f16ad962-557b-4deb-b7ab-626cc52a45f9")
+    .then(response => response.json())
+    .then(data => console.log(data))
